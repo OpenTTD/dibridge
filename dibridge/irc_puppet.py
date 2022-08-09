153733850b9ec1f281fcaa33a1f94e75a6192c2c
Patric Stout <truebrain@openttd.org>
2022-08-09 15:12:27 +0200

    feat(irc): create IRC puppets for every Discord user talking in the channel (#13)
    On IRC this feels more native, as every user talking on Discord
    gets its own entity. This makes things like replies a lot easier.
    
    While at it, also added support for actions, multiline, reply, and
    more.

---
+++ dibridge/irc_puppet.py
@@ -0,0 +1,88 @@
+import asyncio
+import irc.client_aio
+import logging
+
+
+class IRCPuppet(irc.client_aio.AioSimpleIRCClient):
+    def __init__(self, nickname, channel):
+        irc.client.SimpleIRCClient.__init__(self)
+
+        self.loop = asyncio.get_event_loop()
+
+        self._nickname = nickname
+        self._joined = False
+        self._channel = channel
+
+        self._connected_event = asyncio.Event()
+        self._connected_event.clear()
+
+        self._log = logging.getLogger(f"{__name__}.{self._nickname}")
+
+    def on_nicknameinuse(self, client, event):
+        if not self._nickname.endswith("[d]"):
+            self._nickname = f"{self._nickname}[d]"
+            client.nick(self._nickname)
+            return
+
+        if not self._nickname.endswith("~"):
+            self._nickname += "~"
+            client.nick(self._nickname)
+            return
+
+        # TODO -- Other things we can try?
+        self._log.error("Nickname already in use; giving up")
+
+    def on_welcome(self, client, event):
+        self._client = client
+        self._client.join(self._channel)
+
+    def on_privmsg(self, _, event):
+        # TODO -- Consider relaying private messages too. Can be useful to identify with NickServ etc.
+        pass
+
+    # on_pubmsg is done by the IRCRelay, and not by the puppets.
+
+    def on_join(self, _client, event):
+        if event.target != self._channel:
+            return
+
+        if event.source.nick == self._nickname:
+            self._log.info("Joined %s on IRC", self._channel)
+            self._joined = True
+            self._connected_event.set()
+
+    def on_part(self, _client, event):
+        if event.target != self._channel:
+            return
+        self._left(event.source.nick)
+
+    def on_kick(self, _client, event):
+        if event.target != self._channel:
+            return
+        self._left(event.arguments[0])
+
+    def on_disconnect(self, _client, event):
+        self._log.error("Disconnected from IRC: %s", event.arguments[0])
+        self._joined = False
+        self._connected_event.clear()
+        # The library will reconnect us.
+
+    def _left(self, nick):
+        # If we left the channel, rejoin.
+        if nick == self._nickname:
+            self._joined = False
+            self._connected_event.clear()
+            self._client.join(self._channel)
+            return
+
+    async def connect(self, host, port):
+        self._log.info("Connecting to IRC ...")
+        await self.connection.connect(host, port, self._nickname, connect_factory=irc.connection.AioFactory(ssl=True))
+
+    async def send_message(self, content):
+        await self._connected_event.wait()
+        self._client.privmsg(self._channel, content)
+
+    async def send_action(self, content):
+        await self._connected_event.wait()
+        self._client.action(self._channel, content)
